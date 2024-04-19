3fa4d2db521d9811d4220e8842a88d0995a64497
0xdblok <dblok0x@gmail.com>
2024-04-19 22:23:57 +0200

    ee

--- app/component/tokenomics.js
+++ app/component/tokenomics.js
@@ -6,16 +6,12 @@ export default function Tokenomics() {
       <div className="flex pt-16   gap-16 lg:gap-48 ">
         <div className="text-center">
           <h1 className=" text-2xl font-semibold  ">Tokenomics</h1>
-          <p className="pt-6 text-xl">80% LP</p>
-
-          <p className="text-xl ">10% MARKETING</p>
-          <p className="text-xl"> 5% COMMUNITY</p>
-          <p className="text-xl"> 5% TEAM</p>
+          <p className="pt-6 text-xl">100% LOCK LP</p>
         </div>
         <div className="text-center">
           {" "}
           <h1 className=" text-2xl font-semibold">TOTAL SUPPLY</h1>
-          <p className="text-xl pt-6">10,000,000,000</p>
+          <p className="text-xl pt-6">1,000,000,000</p>
         </div>
       </div>
     </div>
